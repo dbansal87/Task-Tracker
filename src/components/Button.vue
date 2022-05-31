3896f5efff6923cc364d91e65e60c2a011ef4383
dbansal87 <106533920+dbansal87@users.noreply.github.com>
2022-05-31 10:16:34 +0530

    Add files of track tracker project using upload

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+  <button class="btn" @click="addTask" :style="{background: color}">
+      {{title}}
+  </button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: ['title','color'],
+    methods: {
+        addTask() {
+            console.log("Clicked");
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+//Entry point of Vue application
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
